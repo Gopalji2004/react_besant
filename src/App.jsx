9ff4138691230513b9279d96963a95eb073a76fa
unknown <Gopalji2004>
2026-06-03 12:21:23 +0530

    name

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 import "./App.css"
 // import { Homecom } from './components/Homecom';
 // import Ticketbook from './components/ticketbook';
-import Apicallone from './components/apicallone';
+// import Apicallone from './components/apicallone';
 
 function App(){
   return(
     <div>
-    {/* <h1 className='count'>Gopalji</h1> */}
-    {/* <h2>Ganga</h2> */}
+    <h1 className='count'>Gopalji</h1>
+    <h2>Ganga</h2>
     
     {/* <Homecom /> */}
     {/* <Ticketbook /> */}
-    <Apicallone />
+    {/* <Apicallone /> */}
     </div>
   )
 }
